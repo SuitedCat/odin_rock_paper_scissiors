cba3edfd7ddc2c7b4886b2c2b9066acd3c62e374
Myah Nix <nixmyah8@gmail.com>
2026-03-09 01:21:05 -0400

    Completed humanChoice function

--- main.js
+++ main.js
@@ -10,14 +10,26 @@ let randomNum =  Math.floor(Math.random() * 3)
 if(randomNum == 1){
     return console.log(r);
 } else if( randomNum == 2){
-    return  console.log(s);;
+    return console.log(s);;
 } else {
     return console.log(p);
 }
 }
 
+function getHumanChoice(input){
+    if(input == "r"){
+    return console.log("UserInput: rocks");
+    } else if(input == "s"){
+    return console.log("UserInput: scissors");
+    } else {
+   return console.log("UserInput: paper");
+    }
+    
+}
+let userInput = prompt("Rock(r) Paper(p) or Scissor(s)?");
+console.log(userInput);
 let tempRo;
 let tempSo;
 let tempPo;
-
+getHumanChoice(userInput);
 getComputerChoice(tempRo,tempSo,tempPo);
